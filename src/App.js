9bd2c0cff9730a096c71808fe7a86cae91dfec6e
akassah1 <akassah1@gmail.com>
2021-10-28 16:56:37 -0700

    assignment two solution

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Assignment2 from './components/Assignment2';
+
+function App() {
+  return (
+    <div className="App">
+      <Assignment2 />
+    </div>
+  );
+}
+
+export default App;
